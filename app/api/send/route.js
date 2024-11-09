f33dd90c29f6c239a73c3ec2445792a0c1faee97
Nipul Yansith <nipulyansith26@gmail.com>
2024-11-09 22:47:53 +0530

    changes

---
+++ app/api/send/route.js
@@ -0,0 +1,23 @@
+// import { EmailTemplate } from '../../../components/EmailTemplate';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'Through My Portfolio <nipulnipul1010@gmail.com>',
+      to: ['nipulyansith26@gmail.com'],
+      subject: 'Hello world',
+      react: <><p>Email Body</p></>,
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
---
+++ app/components/EmailSection.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const EmailSection = () => {
+  return (
+    <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'>
+        <div>
+            <h2 className='text-3xl font-semibold my-2'>Get in Touch</h2>
+            <p className='text-[#ADB7BE]'>I'm currently looking for new opportunities, 
+                my inbox is always open. Whether you have a question or just want to say hi, 
+                I'll try my best to get back to you!
+            </p>
+            <div className='socials flex flex-row gap-2'>
+                
+            </div>    
+        
+        
+        </div>
+    </section>
+  )
+}
+
+export default EmailSection
